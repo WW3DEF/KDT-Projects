8a0cec46763f3966bfb85a0bb1a864dfc77c0783
InDEF <zzangka1122@gmail.com>
2024-09-11 09:24:05 +0900

    DAY5 add && Quiz5 solve

--- projects/JavaStudy/src/Quiz/Q4_1.java
+++ projects/JavaStudy/src/Quiz/Q4_1.java
@@ -7,32 +7,32 @@ public class Q4_1 {
     Scanner sc = new Scanner(System.in);
     String yesOrNo = null;
     boolean stop = false;
-  do {
+    do {
 
 //     과목수 입력
-    System.out.print("과목수 : ");
-    int countOfSubject = sc.nextInt();
+      System.out.print("과목수 : ");
+      int countOfSubject = sc.nextInt();
 
 //    합계 계산
-    int sum = 0; // 합계
-    for (int i = 0; i < countOfSubject; i++) {
-      System.out.println("과목" + (i + 1) + " : ");
-      sum += sc.nextInt();
-    }
+      int sum = 0; // 합계
+      for (int i = 0; i < countOfSubject; i++) {
+        System.out.println("과목" + (i + 1) + " : ");
+        sum += sc.nextInt();
+      }
 
 //    평균 계산
-    double average = ((double)sum / countOfSubject);
+      double average = ((double)sum / countOfSubject);
 
 //  출력 결과
-    System.out.printf("합계 : %d, 평균 : %.2f\n", sum , average);
+      System.out.printf("합계 : %d, 평균 : %.2f\n", sum , average);
 
 //    계속 여부확인
-    System.out.print("계속(y/n) : ");
-    yesOrNo = sc.nextLine();
-    if (!yesOrNo.equalsIgnoreCase("y")) {
-      stop = true;
-      continue;
-    }
+      System.out.print("계속(y/n) : ");
+      yesOrNo = sc.nextLine();
+      if (!yesOrNo.equalsIgnoreCase("y")) {
+        stop = true;
+        continue;
+      }
 //    } while( yesOrNo.equals("y") || yesOrNo.equals("Y") );
     } while(!stop);
   }
