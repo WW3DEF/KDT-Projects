7776680deab59072a290684dbf7a2e650c203396
InDEF <zzangka1122@gmail.com>
2024-09-23 17:32:19 +0900

    ReUpload

--- projects/JavaStudy/src/DAY11/exam5/Main.java
+++ projects/JavaStudy/src/DAY11/exam5/Main.java
@@ -2,15 +2,20 @@ package DAY11.exam5;
 //static
 public class Main {
   public static void main(String[] args) {
+    Person.addCnt();
+    Person.addCnt();
+    System.out.println(Person.cnt); /* 2 */
+
     Person p = new Person();
     p.addAge();
     p.addAge();
+    System.out.println(p.age); /* 2 */
+    System.out.println(p.cnt); /* 2 */
 
     Person p2 = new Person();
     p2.addAge();
 
-    Person.addCnt();
-    Person.addCnt();
-    System.out.println(Person.cnt);
+    System.out.println(p2.age); /* 1 */
+    System.out.println(p2.cnt); /* 2 */
   }
 }
